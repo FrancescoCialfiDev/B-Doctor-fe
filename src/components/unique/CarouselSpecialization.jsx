252a792269951a778c9a9039396df0837d4d368d
CarloDettori <dettoric07@gmail.com>
2025-02-14 18:40:38 +0100

    better icons for specialization carousel

--- src/components/unique/CarouselSpecialization.jsx
+++ src/components/unique/CarouselSpecialization.jsx
@@ -1,9 +1,9 @@
 export default function CarouselSpecialization({ name, image }) {
 
     return (
-        <div className="position-relative p-4 w-100 shadow text-center bg-light">
-            <img src="https://cdn-icons-png.flaticon.com/512/7292/7292483.png" className="img-fluid rounded mx-auto d-block mb-3" alt="carousel-post-img" />
-            <h2 className="position-absolute top-50 start-50 translate-middle fs-3 fw-bold bg-white text-center text-dark">{name}</h2>
+        <div className="position-relative gup-5 p-4 w-100 shadow rounded-circle text-center">
+            <img src="https://static.thenounproject.com/png/761552-200.png" className="img-fluid mx-auto d-block" alt="carousel-post-img" />
+            <p className="position-absolute top-50 start-50 translate-middle fs-4 fw-bold bg-white text-center text-dark">{name}</p>
         </div>
     );
 }
