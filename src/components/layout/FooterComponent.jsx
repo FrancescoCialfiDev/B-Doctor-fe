4c8494855df45757c9bc5a3d23cbb53b01135871
MicheleSaraceni <michele.saraceni23@email.com>
2025-02-14 16:43:32 +0100

    fix footer

--- src/components/layout/FooterComponent.jsx
+++ src/components/layout/FooterComponent.jsx
@@ -1,12 +1,12 @@
 export default function FooterComponent() {
     return (
         <footer className="footer">
-            <p>&copy; 2025 BDoctors. All rights reserved.</p>
-            <p>
+            <div>&copy; 2025 BDoctors. All rights reserved.</div>
+            <div>
                 <a href="#">Privacy Policy</a> |
                 <a href="#">Terms of Service</a> |
                 <a href="#">Contacts</a>
-            </p>
+            </div>
         </footer>
     )
 }
