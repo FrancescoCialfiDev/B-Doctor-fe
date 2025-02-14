c349120ed958f5aa96180a4df81761dcd6216967
alessiotinaglia <alessio.tina1998@gmail.com>
2025-02-14 12:56:52 +0100

    card system

--- src/components/common/CarouselTopFiveDocComponent.jsx
+++ src/components/common/CarouselTopFiveDocComponent.jsx
@@ -1,25 +1,28 @@
-import { GlobalProvider, GlobalContext } from "../../contexts/GlobalContext";
 import { useContext } from "react";
+import { GlobalContext } from "../../contexts/GlobalContext";
 import CarouselTopFiveDoc from "../unique/CarouselTopFiveDoc";
 
-export default function CarouselTopFiveDocComponent({ }) {
-    const { doctors } = useContext(GlobalContext)
-    let topFiveDoc = []
-    for (let i = 0; i < 5; i++) {
-        topFiveDoc.push(doctors[i])
+export default function CarouselTopFiveDocComponent() {
+    const { doctors } = useContext(GlobalContext);
+
+    if (!doctors || doctors.length === 0) {
+        return <p>No doctors available</p>;
     }
-    
+
+    let topFiveDoc = doctors.slice(0, 5);
+
     return (
         <div className="overflow-auto my-5">
             <div className="d-flex w-100">
                 {topFiveDoc.map((doctor) => {
+                    if (!doctor?.id) return null;
                     return (
                         <CarouselTopFiveDoc
+                            key={doctor?.id}
                             id={doctor?.id}
                             name={doctor?.name}
                             surname={doctor?.surname}
-                            specialization={doctor?.specialization}
-                            // image={doctor.img}
+                            specialization={doctor?.specializations}
                             vote={doctor?.vote}
                         />
                     );
@@ -27,4 +30,4 @@ export default function CarouselTopFiveDocComponent({ }) {
             </div>
         </div>
     );
-}    
+}
