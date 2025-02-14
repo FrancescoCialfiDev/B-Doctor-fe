ecbc8f9db4d9895c4e81aa25a184c36602c6ea2c
MicheleSaraceni <michele.saraceni23@email.com>
2025-02-14 16:41:01 +0100

    Added standard Footer

--- src/components/layout/FooterComponent.jsx
+++ src/components/layout/FooterComponent.jsx
@@ -1,7 +1,12 @@
 export default function FooterComponent() {
     return (
-        <>
-            <h1>Sono l'Footer</h1>
-        </>
+        <footer className="footer">
+            <p>&copy; 2025 BDoctors. All rights reserved.</p>
+            <p>
+                <a href="#">Privacy Policy</a> |
+                <a href="#">Terms of Service</a> |
+                <a href="#">Contacts</a>
+            </p>
+        </footer>
     )
 }
