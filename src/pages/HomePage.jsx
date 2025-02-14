8b0a8cff57eef611286fb7b566762b8c4d42ab5a
alessiotinaglia <alessio.tina1998@gmail.com>
2025-02-14 15:29:41 +0100

    create intro card home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,29 @@
-import { GlobalContext } from "../contexts/GlobalContext.jsx"
-import { useContext } from "react"
 import CarouselTopFiveDocComponent from "../components/common/CarouselTopFiveDocComponent.jsx"
+import style from "./HomePage.module.css";
 
-export default function HomePage() {    
+
+export default function HomePage() {
     return (
-        <>
-            <CarouselTopFiveDocComponent/>
-        </>
+        <section className="px-3">
+            <div className="card pt-3" style={{ width: '100%', borderRadius: '25px' }}>
+                <div className="row">
+                    <div className="col-6">
+                        <img src="https://pngimg.com/d/doctor_PNG15964.png" className="card-img-top ps-2" alt="..." />
+                    </div>
+
+                    <div className="col-6">
+                        <div>
+                            <h2 className="card-title pt-2">Yout Healt is our Priority</h2>
+                            <h6 className="card-text">Dr: Antony</h6>
+                            <p className={style.textSmall}>Ortophedic | Specialist</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <CarouselTopFiveDocComponent />
+        </section>
+
     )
 }
