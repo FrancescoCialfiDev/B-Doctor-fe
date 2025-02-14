38d764f3a0be5771ee373bd38462f19e3a393400
CarloDettori <dettoric07@gmail.com>
2025-02-14 18:23:27 +0100

    giving bg to specialization carousel name inside the card

--- src/components/unique/CarouselSpecialization.jsx
+++ src/components/unique/CarouselSpecialization.jsx
@@ -3,7 +3,7 @@ export default function CarouselSpecialization({ name, image }) {
     return (
         <div className="position-relative p-4 w-100 shadow text-center bg-light">
             <img src="https://cdn-icons-png.flaticon.com/512/7292/7292483.png" className="img-fluid rounded mx-auto d-block mb-3" alt="carousel-post-img" />
-            <h2 className="position-absolute top-50 start-50 translate-middle fs-3 fw-bold text-center text-dark">{name}</h2>
+            <h2 className="position-absolute top-50 start-50 translate-middle fs-3 fw-bold bg-white text-center text-dark">{name}</h2>
         </div>
     );
 }
