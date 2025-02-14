662bc4af4e09fad17a306db64c5439ae416cdef7
MicheleSaraceni <michele.saraceni23@email.com>
2025-02-14 16:10:35 +0100

    Added stars for reviews

--- src/components/common/reviews/ReviewCard.jsx
+++ src/components/common/reviews/ReviewCard.jsx
@@ -1,5 +1,5 @@
 import styles from "./ReviewCard.module.css";
-
+import StarsComponent from "../StarsComponent";
 
 export default function ReviewCard({ review }) {
 
@@ -9,7 +9,9 @@ export default function ReviewCard({ review }) {
                 <div className={styles.review_card}>
                     <div className={styles.review_header}>
                         <span>{review?.name_patient}</span>
-                        <span className={styles.stars}>★★★★★</span>
+                        <span className={styles.stars}>
+                            <StarsComponent vote={review?.vote} />
+                        </span>
                     </div>
                     <div className={styles.review_body}>
                         <p>{review?.description}</p>
