9a8885c1c1d153ce345ddc6d2f8c9688d00d4328
CarloDettori <dettoric07@gmail.com>
2025-02-16 19:21:50 +0100

    better layout for doctors page

--- src/pages/DoctorsPage.jsx
+++ src/pages/DoctorsPage.jsx
@@ -8,22 +8,25 @@ export default function DoctorsPage() {
     const { doctors } = useContext(GlobalContext);
 
     return (
-        <div className={style.container}>
-            {doctors
-                .map((doctor) => (
-                    <div key={doctor.id} className={style.doctorCard}>
-                        <img className={style.image} src={doctor.img_url} alt={`${doctor.name} ${doctor.surname}`} />
-                        <div className={style.details}>
-                            <h3>{doctor.name} {doctor.surname}</h3>
-                            <p className={style.stars}><StarsComponent vote={doctor.vote_average} /></p>
-                            <div className={style.specializations}>
-                                <h4>{doctor.specializations}</h4>
-                                <Link to={`/doctors/${doctor.id}`}>View Profile</Link>
+        <>
+            <div className={style.container}>
+                {doctors
+                    .map((doctor) => (
+                        <div key={doctor.id} className={style.doctorCard}>
+                            <img className={style.image} src={doctor.img_url} alt={`${doctor.name} ${doctor.surname}`} />
+                            <div className={style.details}>
+                                <h3>{doctor.name} {doctor.surname}</h3>
+                                <p className={style.stars}><StarsComponent vote={doctor.vote_average} /></p>
+                                <div className={style.specializations}>
+                                    <h4>{doctor.specializations}</h4>
+                                    <Link to={`/doctors/${doctor.id}`}>View Profile</Link>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+
+            </div>
             <Link to="/formDoctor" className="nav-link">Sing in Now</Link>
-        </div>
+        </>
     );
 }
