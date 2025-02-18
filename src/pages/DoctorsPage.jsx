bd1462f5b5205cc39d8706559d6f7bed5addeceb
alessiotinaglia <alessio.tina1998@gmail.com>
2025-02-18 13:05:03 +0100

    finished part card doctorsPage

--- src/pages/DoctorsPage.jsx
+++ src/pages/DoctorsPage.jsx
@@ -10,35 +10,35 @@ export default function DoctorsPage() {
     return (
         <>
             <h1 className="text-center m-3">Doctor List</h1>
-            <div className={style.badgeContainer}>
-                {doctors
-                    .map((doctor) => (
-                        <div key={doctor.id} className={style.badge}>
-                            <div className={style.clip}></div>
-                            <div className={style.header}>
-                                <h3 className={style.doctorName}>{doctor.name} {doctor.surname}</h3>
-                            </div>
-                            <div className={style.profileContainer}>
-                                <img className={style.profileImage} src={doctor.img_url} alt={`${doctor.name} ${doctor.surname}`} />
-                                                                
-                            </div>
-                            <div className={style.vote}>
+            <div className="container">
+                <div className="row justify-content-center">
+                    {doctors.map((doctor) => (
+                        <div key={doctor.id} className="col-lg-6 col-12 d-flex justify-content-center">
+                            <div className={`card p-3 shadow-lg ${style.badge}`}>
+                                <div className="card-header text-white text-center bg-primary">
+                                    <h3 className={`mb-0 ${style.title}`}>{doctor.name} {doctor.surname}</h3>
+                                </div>
+                                <div className="d-flex justify-content-center position-relative">
+                                    <img className={style.profileImage} src={doctor.img_url} alt={`${doctor.name} ${doctor.surname}`} />
+                                </div>
+                                <div className="text-center mt-2">
                                     <StarsComponent vote={doctor.vote_average} />
                                 </div>
-                            <div className={style.detailsSection}>
-                                <p><strong>Specializations:</strong> {doctor.specializations}</p>
-                                {/* <p>{doctor?.icon_url}</p> */}
-                            </div>
-                            <div className={style.signatureSection}>
-                                <Link to={`/doctors/${doctor.id}`} className={style.viewProfileLink}>View Profile</Link>
+                                <div className="card-body text-center ">
+                                    <p><strong>Specializations:</strong> {doctor.specializations}</p>
+                                </div>
+                                <div className="text-center">
+                                    <Link to={`/doctors/${doctor.id}`} className="btn btn-primary">View Profile</Link>
+                                </div>
                             </div>
                         </div>
                     ))}
-                <div className="d-flex justify-content-center">
-                    <Link to="/formDoctor" className="btn btn-primary m-3">
-                        Se sei un dottore, puoi registrarti qui
-                    </Link>
                 </div>
+            </div>
+            <div className="d-flex justify-content-center">
+                <Link to="/formDoctor" className="btn btn-primary m-3">
+                    Se sei un dottore, puoi registrarti qui
+                </Link>
             </div>
         </>
     );
