d3ed95e3914ff9553da5294015b8bb2664a4cb3b
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2025-02-22 15:47:41 +0100

    changed

--- src/components/common/RealCarouselComponent.jsx
+++ src/components/common/RealCarouselComponent.jsx
@@ -16,46 +16,50 @@ export default function RealCarouselComponent() {
 
     return (
 
-        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <div id="carouselExampleCaptions" className="carousel slide position-relative" data-bs-ride="carousel">
+            <div>
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div className="carousel-inner ">
+                    <div className={`carousel-item active ${style.cardBackground}`}>
+                        <img src={heroImg1} className="jumboImg d-block w-100" alt="First slide" />
+                        <div className="carousel-caption d-none d-md-block z-1 position-absolute">
+                            <h2 className="text-white card-title pt-2">Your Health, Our Mission – Innovative Care for a Better Tomorrow</h2>
+                            <h6 className="text-white card-text">Combining expertise and technology to ensure the best treatments and faster recovery</h6>
+                            <p className={style.textSmall}>Because every step towards healing starts with the right care and dedication.</p>
+                        </div>
+                    </div>
+                    <div className={`carousel-item ${style.cardBackground}`}>
+                        <img src={heroImg3} className={`jumboImg d-block w-100`} alt="Second slide" />
+                        <div className="carousel-caption d-none d-md-block z-1">
+                            <h2 className="text-white card-title pt-2">Your Health, Our Mission – Innovative Care for a Better Tomorrow</h2>
+                            <h6 className="text-white card-text">Combining expertise and technology to ensure the best treatments and faster recovery</h6>
+                            <p className={style.textSmall}>Because every step towards healing starts with the right care and dedication.</p>
+                        </div>
+                    </div>
+                    <div className={`carousel-item ${style.cardBackground}`}>
+                        <img src={heroImg2} className="  jumboImg d-block w-100" alt="Third slide" />
+                        <div className="carousel-caption d-none d-md-block z-1">
+                            <h2 className="text-white card-title pt-2">Your Health, Our Mission – Innovative Care for a Better Tomorrow</h2>
+                            <h6 className="text-white card-text">Combining expertise and technology to ensure the best treatments and faster recovery</h6>
+                            <p className={style.textSmall}>Because every step towards healing starts with the right care and dedication.</p>
+                        </div>
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
             </div>
-            <div className="carousel-inner">
-                <div className={`carousel-item active ${style.cardBackground}`}>
-                    <img src={heroImg1} className=" jumboImg d-block w-100" alt="First slide" />
-                    <div className="carousel-caption d-none d-md-block">
-                        <h2 className="text-white card-title pt-2">Your Health, Our Mission – Innovative Care for a Better Tomorrow</h2>
-                        <h6 className="text-white card-text">Combining expertise and technology to ensure the best treatments and faster recovery</h6>
-                        <p className={style.textSmall}>Because every step towards healing starts with the right care and dedication.</p>
-                    </div>
-                </div>
-                <div className={`carousel-item ${style.cardBackground}`}>
-                    <img src={heroImg3} className={`${style.jumboImg}  d-block w-100`} alt="Second slide" />
-                    <div className="carousel-caption d-none d-md-block">
-                        <h2 className="text-white card-title pt-2">Your Health, Our Mission – Innovative Care for a Better Tomorrow</h2>
-                        <h6 className="text-white card-text">Combining expertise and technology to ensure the best treatments and faster recovery</h6>
-                        <p className={style.textSmall}>Because every step towards healing starts with the right care and dedication.</p>
-                    </div>
-                </div>
-                <div className={`carousel-item ${style.cardBackground}`}>
-                    <img src={heroImg2} className="  jumboImg d-block w-100" alt="Third slide" />
-                    <div className="carousel-caption d-none d-md-block">
-                        <h2 className="text-white card-title pt-2">Your Health, Our Mission – Innovative Care for a Better Tomorrow</h2>
-                        <h6 className="text-white card-text">Combining expertise and technology to ensure the best treatments and faster recovery</h6>
-                        <p className={style.textSmall}>Because every step towards healing starts with the right care and dedication.</p>
-                    </div>
-                </div>
+            <div id='overlay' className=" position-absolute top-0 h-100 w-100">
             </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
         </div>
 
     );
